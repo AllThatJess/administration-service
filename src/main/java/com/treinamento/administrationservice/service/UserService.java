8159b9b80c27f083189daa6110f3bc7fec5b8426
Jessica Alexandra Rodrigues dos Santos <j.alexandra.r61@gmail.com>
2021-02-26 19:35:02 -0300

    Novo commit

---
+++ src/main/java/com/treinamento/administrationservice/service/UserService.java
@@ -0,0 +1,32 @@
+package com.treinamento.administrationservice.service;
+
+import com.treinamento.administrationservice.entity.User;
+import com.treinamento.administrationservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repo;
+
+    public List<User> findAll(){
+
+        return this.repo.findAll();
+    }
+
+    public User findById(Integer id) throws Exception{
+
+        Optional<User> userExisted = this.repo.findById(id);
+
+        if (userExisted.isPresent()) {
+            return userExisted.get();
+        }else {
+            throw new Exception("User not found: " + id);
+        }
+    }
+}
